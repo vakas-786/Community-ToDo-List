69bd796f6cc6803d67cf23287fb8fed7da7d9440
vakas-786 <vakas0959@gmail.com>
2020-10-12 22:37:34 -0400

    Procfile

---
+++ build/precache-manifest.78db56cae5ba3f552c3964c44d3af572.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984444d279563dfa40e9b2b259741e56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f6b4c81ccd317471330",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "47c34639582a76fc5be0",
+    "url": "/static/css/main.86c5206d.chunk.css"
+  },
+  {
+    "revision": "7f6b4c81ccd317471330",
+    "url": "/static/js/2.b45bfcd6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b45bfcd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c34639582a76fc5be0",
+    "url": "/static/js/main.ad6fc71b.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  },
+  {
+    "revision": "88bca37507ab1b668947fdc1ae93cf3f",
+    "url": "/static/media/85281e34521c4dbe0a96a3f35ff798ca.88bca375.gif"
+  }
+]);
